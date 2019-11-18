c25d216a29a3305a796a22ee9857622ad4b79c9b
Zach Foster <wormflakes@me.com>
2019-11-17 16:07:30 -0800

    Refactored NewTaskModal a bit.

--- lib/new_task_modal.dart
+++ lib/new_task_modal.dart
@@ -13,54 +13,8 @@ class _NewTaskModalState extends State<NewTaskModal> {
       FocusNode(debugLabel: 'Task details focused');
   bool _detailsAdded = false;
   bool _detailsInitiallyFocused = false;
-
   String _taskText = '';
   String _detailText = '';
-
-  @override
-  void dispose() {
-    _taskDetailsFocusNode.dispose();
-    super.dispose();
-  }
-
-  _addDetails() {
-    setState(() {
-      _detailsAdded = true;
-    });
-  }
-
-  _setupFocus() {
-    if (_detailsAdded && !_detailsInitiallyFocused) {
-      FocusScope.of(context).requestFocus(_taskDetailsFocusNode);
-      setState(() {
-        _detailsInitiallyFocused = true;
-      });
-    }
-  }
-
-  _updateTaskText(String text) {
-    print('task: $text');
-
-    setState(() {
-      _taskText = text;
-    });
-  }
-
-  _updateDetailText(String text) {
-    print('details: $text');
-
-    setState(() {
-      _detailText = text;
-    });
-  }
-
-  _createTask() {
-    if (_taskText.isNotEmpty | !_detailText.isNotEmpty) {
-      Provider.of<TaskListModel>(context, listen: false)
-          .createTask(_taskText, details: _detailText);
-    }
-    Navigator.pop(context);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,75 +28,110 @@ class _NewTaskModalState extends State<NewTaskModal> {
         ),
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child: Form(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'New task',
-                      border: InputBorder.none,
-                    ),
-                    autofocus: true,
-                    onChanged: _updateTaskText,
-                    onFieldSubmitted: (_) => _createTask(),
-                    validator: (value) => 'Enter something',
-                    // Close modal on submit.
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'New task',
+                    border: InputBorder.none,
                   ),
+                  autofocus: true,
+                  onChanged: _setTaskText,
+                  onSubmitted: (_) => _createTask(),
                 ),
-                _detailsAdded
-                    ? Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: TextFormField(
-                          decoration: InputDecoration.collapsed(
-                            hintText: 'Add details',
-                            border: InputBorder.none,
-                          ),
-                          focusNode: _taskDetailsFocusNode,
-                          style: TextStyle(fontSize: 14),
-                          maxLines: null,
-                          onChanged: _updateDetailText,
-//                    validator: (value) => 'Enter something',
-                          // Close modal on submit.
+              ),
+              _detailsAdded
+                  ? Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: TextField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Add details',
+                          border: InputBorder.none,
                         ),
-                      )
-                    : Container(),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Row(
-                    children: <Widget>[
-                      IconButton(
-                        padding: EdgeInsets.zero,
-                        icon: Icon(Icons.subject),
-                        color: Colors.blue,
-                        onPressed: _detailsAdded ? null : _addDetails,
+                        focusNode: _taskDetailsFocusNode,
+                        style: TextStyle(fontSize: 14),
+                        maxLines: null,
+                        onChanged: _setDetailText,
                       ),
-                      IconButton(
-                        padding: EdgeInsets.zero,
-                        icon: Icon(Icons.event_available),
-                        color: Colors.blue,
-                        onPressed: null,
+                    )
+                  : Container(),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(Icons.subject),
+                      color: Colors.blue,
+                      onPressed: _detailsAdded ? null : _addDetails,
+                    ),
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(Icons.event_available),
+                      color: Colors.blue,
+                      onPressed: null,
+                    ),
+                    Spacer(),
+                    FlatButton(
+                      child: Text(
+                        'Save',
+                        style: Styles.buttonTextStyle,
                       ),
-                      Spacer(),
-                      FlatButton(
-                        child: Text(
-                          'Save',
-                          style: Styles.buttonTextStyle,
-                        ),
 //                    color: Colors.blue,
-                        onPressed: _createTask,
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                      onPressed: _createTask,
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+
+  _setupFocus() {
+    if (_detailsAdded && !_detailsInitiallyFocused) {
+      FocusScope.of(context).requestFocus(_taskDetailsFocusNode);
+      setState(() {
+        _detailsInitiallyFocused = true;
+      });
+    }
+  }
+
+  _addDetails() {
+    setState(() {
+      _detailsAdded = true;
+    });
+  }
+
+  _setTaskText(String text) {
+    setState(() {
+      _taskText = text;
+    });
+  }
+
+  _setDetailText(String text) {
+    setState(() {
+      _detailText = text;
+    });
+  }
+
+  _createTask() {
+    if (_taskText.isNotEmpty | _detailText.isNotEmpty) {
+      Provider.of<TaskListModel>(context, listen: false)
+          .createTask(_taskText, details: _detailText);
+    }
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _taskDetailsFocusNode.dispose();
+    super.dispose();
   }
 }
